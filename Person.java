c95da5e0426bcfc4f69a65b200f4f41f174baa33
mbowman21 <sstraberry101@gmail.com>
2023-04-06 16:32:44 -0500

    created person class

---
+++ Person.java
@@ -0,0 +1,58 @@
+import java.util.Date;
+
+abstract class Person {
+    String name;
+    String address;
+    Date dob;
+    String email;
+    int ssn;
+
+    public String toString(){
+
+    }
+
+    public void setName(String name) {
+
+    }
+
+    public void setAddress(String address) {
+
+    }
+    public void setDate(Date) {
+
+    }
+    
+    public void setEmail(String) {
+
+    }
+
+    public void setSSN(SSN) {
+
+    }
+
+    public String getName() {
+
+    }
+
+    public String getAddress() {
+
+    }
+
+    public Date getDate() {
+
+    }
+
+    public String getEmail() {
+
+    }
+
+    public int getSSN() {
+
+    }
+
+    public String getSSN(){
+
+    }
+
+    
+}
