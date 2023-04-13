ea9decef155ed7db0b563667b27a63625472f4d3
mbowman21 <sstraberry101@gmail.com>
2023-04-13 16:02:12 -0500

    added member methods

--- Member.java
+++ Member.java
@@ -1,4 +1,20 @@
 abstract class Member extends Person{
     protected int MemberID;
     protected float Balance;
+
+    public void setMemberId(int id) {
+        this.MemberID = id;
+    }
+
+    public void setBalance(float balance) {
+        this.Balance = balance;
+    }
+
+    public int getMemberId() {
+        return this.MemberID;
+    }
+
+    public float getBalance() {
+        return this.Balance;
+    }
 }
