38a03d8fd9d7c10a960c11b0a3b7201d9869906d
Noah Fouts <nfouts2205@gmail.com>
2023-04-13 11:53:46 -0500

    Added course, item class and student subclass

---
+++ Course.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.plaf.InputMapUIResource;
+
+abstract class Course {
+    protected String courseName;
+    protected String courseCode;
+
+    protected int studentCount;
+    protected int courseCredit;
+    protected Professor professor;
+    protected ArrayList<Item> requiredMaterials;
+    protected ArrayList<Student> roster;
+
+    // default constructor
+    public Course(String courseName, String courseCode, Professor courseProfessor, ArrayList<Item> requiredMaterials, int creditAmount, ArrayList<Student> roster){
+        this.requiredMaterials = requiredMaterials;
+        setCourseCode(courseCode);
+        setCourseName(courseName);
+        setCourseCredit(creditAmount);
+        //roster can be nil
+        setRoster(roster);
+
+        this.studentCount = roster.size();
+    }
+
+    // credit functions
+    public int getCourseCredit(){
+        return this.courseCredit;
+    }
+
+    public void setCourseCredit(int creditAmount){
+        this.courseCredit = creditAmount;
+    }
+
+    // roster functions
+    public ArrayList<Student> getRoster(){
+        return roster;
+    }
+
+    public void setRoster(ArrayList<Student> roster){
+        this.roster = roster;
+    }
+
+    // required materials functions
+    public ArrayList<Item> getRequiredMaterials(){
+        return this.requiredMaterials;
+    }
+
+    public void setRequiredMaterials(ArrayList<Item> requiredMaterials){
+        //assuming requiredMaterials extends to not only books
+        this.requiredMaterials = requiredMaterials;
+    }
+
+    // course name functions
+    public String getCourseName(){
+        return courseName;
+    }
+
+    public void setCourseName(String courseName){
+        this.courseName = courseName;
+    }
+
+    // course code functions
+    public String getCourseCode(){
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode){
+        this.courseCode = courseCode;
+    }
+
+    // professor functions
+    public void setProffessor(Professor professor){
+        this.professor = professor;
+    }
+
+    public Professor getProfessor(){
+        return professor;
+    }
+
+    // student functions
+    public Student getStudent(int studentId){
+        
+    }
+}
--- Student.java
+++ Student.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Student extends Employee{
-    
+    protected int studentId;
+    protected ArrayList<Course> courses;
+
+    public Student(String name, Date DOB, String studentEmail, int studentId){
+        setName(name);
+        setDateOfBirth(DOB);
+        setEmail(studentEmail);
+        setStudentId(studentId);
+    }
+
+    public void addCourse(Course course){
+        courses.add(course);
+    }
+
+    public void removeCourse(Course course){
+        //add catch for if a course does not exist
+        courses.remove(course);
+    }
+
+    public int getStudentId(){
+        return this.studentId;
+    }
+
+    public void setStudentId(int studentId){
+        this.studentId = studentId;
+    }
 }
