83413e495c5671c3784488272c150ce0352ce4cd
mbowman21 <sstraberry101@gmail.com>
2023-04-17 13:34:52 -0500

    created a constructor for librarian

--- Librarian.java
+++ Librarian.java
@@ -1,4 +1,16 @@
+import java.util.Date;
 public class Librarian extends Employee {
+    public Librarian(String name, String address, Date dob, String email, long ssn, int employeeId, Date startDate, float salary) {
+        setName(name);
+        setAddress(address);
+        setDateOfBirth(startDate);
+        setEmail(email);
+        setSSN(ssn);
+        setEmployeeId(employeeId);
+        setStartDate(dob);
+        setSalary(salary);
+
+    }
 
     public Book newBook(String type, String section, String title, String author, String publisher, int itemID){
         Book newBook = new Book(section, title, author, publisher, itemID);
