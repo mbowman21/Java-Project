0e244691677aedf9ae366fcd6dc1a104f6ff642d
mbowman21 <sstraberry101@gmail.com>
2023-05-06 18:20:12 -0500

    added comments and added check for ssn

--- NewMembership.java
+++ NewMembership.java
@@ -3,25 +3,30 @@ import java.awt.event.*;
 import java.time.*;
 
 public class NewMembership {
+    // creates text fields
     public static void createTextField (JTextField textField, JFrame frame, int y) {
         textField.setBounds(50,y,150,30);  
         frame.add(textField);
     }
 
+    // creates labels
     public static void createLabel(JLabel label, JFrame frame, int y) {
         label.setBounds(50, y, 150, 20);
         frame.add(label);
     }
 
+    // function that'll change to the new membership page
     public static void changeToNewMembership(JFrame oldFrame){
         oldFrame.setVisible(false);
         newMembership();
     }
     
+    // new membership frontend and backend
     public static void newMembership() {
         JFrame f = new JFrame("New Membership Creation");
         f.setVisible(true);
 
+        //labels and inputs for things
         JLabel nameLabel = new JLabel("Name:");
         JLabel emailLabel = new JLabel("Email:");
         JLabel idLabel = new JLabel("Id:");
@@ -51,7 +56,7 @@ public class NewMembership {
 
         JButton submit = new JButton("Submit");
 
-
+        // creating and adding all of the labels and inputs to the page
         JButton menuButton = new JButton("Menu");
         menuButton.setBounds(0, 0, 75, 20);
         f.add(menuButton);
@@ -100,6 +105,7 @@ public class NewMembership {
         submit.setBounds(50, 590, 150, 30);
         f.add(submit);
 
+        // when the user hits submit it will try and add the member to the memberlist but if there was an error in inputting it then it will send a popup that will tell the user there is a problem
         submit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
                 try {
@@ -124,7 +130,9 @@ public class NewMembership {
 
                     if (typeInput.getItemAt(typeInput.getSelectedIndex()) == "Student") {
                         for (Student student : Main.studentList) {
-                            if (student.MemberID == id)
+                            if (student.getMemberId() == id)
+                                throw new Exception();
+                            if (student.getSSN() == ssn)
                                 throw new Exception();
                         }
                         Student newStudent = new Student(name, dob, email, id, address, ssn);
@@ -134,7 +142,9 @@ public class NewMembership {
                     
                     else if (typeInput.getItemAt(typeInput.getSelectedIndex()) == "Professor") {
                         for (Professor professor : Main.professorList) {
-                            if (professor.MemberID == id)
+                            if (professor.getMemberId() == id)
+                                throw new Exception();
+                            if (professor.getSSN() == ssn)
                                 throw new Exception();
                         }
                         Professor newProfessor = new Professor(name, dob, email, id, address, ssn);
@@ -144,7 +154,9 @@ public class NewMembership {
                     
                     else if (typeInput.getItemAt(typeInput.getSelectedIndex()) == "External") {
                         for (External external : Main.externalList) {
-                            if (external.MemberID == id)
+                            if (external.getMemberId() == id)
+                                throw new Exception();
+                            if (external.getSSN() == ssn)
                                 throw new Exception();
                         }
                         External newExternal = new External(name, dob, email, id, address, ssn);
@@ -156,7 +168,7 @@ public class NewMembership {
                         throw new Exception();
                 } catch(Exception e) {
                     System.out.println(e);
-                    JOptionPane.showMessageDialog(f,"Error! One of the items you inputted was not formatted correctly or that id is already in use. Please try again.");
+                    JOptionPane.showMessageDialog(f,"Error! One of the items you inputted was formatted incorrectly and/or that id/ssn is already in use. Please try again.");
                 }
             }
         });
