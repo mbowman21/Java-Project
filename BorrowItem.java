71dbe052b1c3988b8c926a764f083cddaad2bcc8
andrewp5901 <130093325+andrewp5901@users.noreply.github.com>
2023-05-10 21:08:32 -0500

    Update BorrowItem.java

--- BorrowItem.java
+++ BorrowItem.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class BorrowItem {
+
     public static void changeToBorrowItem(JFrame oldFrame) {
         oldFrame.setVisible(false);
         borrowItem();
@@ -10,6 +11,15 @@ public class BorrowItem {
     public static void borrowItem() {
         JFrame f = new JFrame("Borrow Item");
         f.setVisible(true);
+
+        JLabel itemLabel = new JLabel("Item Type:");
+        String items[] = {"Book", "Dvd", "Journal", "Newspaper"};
+        JComboBox<String> itemInput = new JComboBox<>(items);
+
+        JLabel idLabel = new JLabel("Item ID: ");
+        JTextField idInput = new JTextField();
+
+        JButton submitBorrowButton = new JButton("Submit Borrow");
 
         JButton menuButton = new JButton("Menu");
         menuButton.setBounds(0, 0, 75, 20);
@@ -20,6 +30,39 @@ public class BorrowItem {
             }   
         });
         
+        itemLabel.setBounds(50, 30, 150, 20);
+        f.add(itemLabel);
+        itemInput.setBounds(50, 50, 150, 30);
+        f.add(itemInput);
+
+        idLabel.setBounds(50, 80, 150, 20);
+        f.add(idLabel);
+        idInput.setBounds(50, 100, 150, 20);
+        f.add(idInput);
+
+        submitBorrowButton.setBounds(50, 200, 150, 30);
+        f.add(submitBorrowButton);
+
+        submitBorrowButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                try {
+
+                    String item = itemInput.getItemAt(itemInput.getSelectedIndex());
+                    long itemID = Long.parseLong(idInput.getText());
+
+                    if (item.isEmpty())
+                        throw new Exception();
+
+                    if (itemID < 0 || itemID > 10000000000L)
+                        throw new Exception();
+
+                } catch(Exception e) {
+                    System.out.println(e);
+                    JOptionPane.showMessageDialog(f,"Error! Please try again.");
+                }
+            }
+        });
+
 
         f.setSize(300,750);  
         f.setLayout(null);  
