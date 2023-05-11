d956d58f4df7b6c19a616292d7243dff456d2b10
andrewp5901 <130093325+andrewp5901@users.noreply.github.com>
2023-05-11 18:09:12 -0500

    Update BorrowItem.java

--- BorrowItem.java
+++ BorrowItem.java
@@ -1,71 +1,137 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 public class BorrowItem {
-
+    
     public static void changeToBorrowItem(JFrame oldFrame) {
+        int oldFrameWidth = oldFrame.getWidth();
+        int oldFrameHeight = oldFrame.getHeight();
         oldFrame.setVisible(false);
-        borrowItem();
+        borrowItem(oldFrameWidth, oldFrameHeight);
     }
 
-    public static void borrowItem() {
+    public static void borrowItem(int frameWidth, int frameHeight) {
         JFrame f = new JFrame("Borrow Item");
-        f.setVisible(true);
+        f.setSize(frameWidth, frameHeight);
 
-        JLabel itemLabel = new JLabel("Item Type:");
-        String items[] = {"Book", "Dvd", "Journal", "Newspaper"};
-        JComboBox<String> itemInput = new JComboBox<>(items);
+        JLabel collectionLabel = new JLabel("Collection:");
+        String collections[] = {"Book", "Dvd", "Journal", "Newspaper"};
+        JComboBox<String> collectionInput = new JComboBox<>(collections);
 
-        JLabel idLabel = new JLabel("Item ID: ");
-        JTextField idInput = new JTextField();
+        JLabel itemIdLabel = new JLabel("Enter the item's ID:");
+        JTextField itemIdInput = new JTextField();
 
-        JButton submitBorrowButton = new JButton("Submit Borrow");
+        JLabel memberIdLabel = new JLabel("Enter your Member ID:");
+        JTextField memberIdInput = new JTextField();
+
+        JButton submitButton = new JButton("Submit");
 
         JButton menuButton = new JButton("Menu");
         menuButton.setBounds(0, 0, 75, 20);
         f.add(menuButton);
         menuButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 StartMenu.changeToStartMenu(f);
-            }   
+            }
         });
-        
-        itemLabel.setBounds(50, 30, 150, 20);
-        f.add(itemLabel);
-        itemInput.setBounds(50, 50, 150, 30);
-        f.add(itemInput);
 
-        idLabel.setBounds(50, 80, 150, 20);
-        f.add(idLabel);
-        idInput.setBounds(50, 100, 150, 20);
-        f.add(idInput);
+        collectionLabel.setBounds(50, 30, 150, 20);
+        f.add(collectionLabel);
+        collectionInput.setBounds(50, 50, 200, 30);
+        f.add(collectionInput);
 
-        submitBorrowButton.setBounds(50, 200, 150, 30);
-        f.add(submitBorrowButton);
+        itemIdLabel.setBounds(50, 80, 250, 20);
+        f.add(itemIdLabel);
+        itemIdInput.setBounds(50, 100, 300, 30);
+        f.add(itemIdInput);
 
-        submitBorrowButton.addActionListener(new ActionListener() {
+        memberIdLabel.setBounds(50, 130, 250, 20);
+        f.add(memberIdLabel);
+        memberIdInput.setBounds(50, 150, 300, 30);
+        f.add(memberIdInput);
+
+        submitButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
                 try {
+                    String collection = collectionInput.getItemAt(collectionInput.getSelectedIndex());
+                    long itemId = Long.parseLong(itemIdInput.getText());
+                    int memberId = Integer.parseInt(memberIdInput.getText());
+                    
+                    Member borrower = null;
+                    ArrayList<Member> allMembers = new ArrayList<>();
+                    allMembers.addAll(Main.professorList);
+                    allMembers.addAll(Main.studentList);
+                    allMembers.addAll(Main.externalList);
+                    for (Member member : allMembers) {
+                        if (member.getMemberId() == memberId) {
+                            borrower = member;
+                            break;
+                        }
+                    }
+                    
+                    if (borrower == null) {
+                        throw new Exception("Member not found.");
+                    }
 
-                    String item = itemInput.getItemAt(itemInput.getSelectedIndex());
-                    long itemID = Long.parseLong(idInput.getText());
+                    Item itemToBorrow = null;
+                    switch(collection) {
+                        case "Book":
+                            for (Book book : Main.bookList) {
+                                if (book.getItemID() == itemId) {
+                                    itemToBorrow = book;
+                                    break;
+                                }
+                            }
+                            break;
+                        case "Dvd":
+                            for (Dvd dvd : Main.dvdList) {
+                                if (dvd.getItemID() == itemId) {
+                                    itemToBorrow = dvd;
+                                    break;
+                                }
+                            }
+                            break;
+                        case "Newspaper":
+                            for (Newspaper newspaper : Main.newspaperList) {
+                                if (newspaper.getItemID() == itemId) {
+                                    itemToBorrow = newspaper;
+                                    break;
+                                }
+                            }
+                            break;
+                        case "Journal":
+                            for (Journal journal : Main.journalList) {
+                                if (journal.getItemID() == itemId) {
+                                    itemToBorrow = journal;
+                                    break;
+                                }
+                            }
+                            break;
+                    }
 
-                    if (item.isEmpty())
-                        throw new Exception();
+                    if (itemToBorrow == null) {
+                        throw new Exception("Item not found.");
+                    }
 
-                    if (itemID < 0 || itemID > 10000000000L)
-                        throw new Exception();
+                    itemToBorrow.checkout(borrower);
+                    borrower.checkoutItem(itemToBorrow);
+
+                    JOptionPane.showMessageDialog(f,"Item successfully borrowed!");
 
                 } catch(Exception e) {
                     System.out.println(e);
-                    JOptionPane.showMessageDialog(f,"Error! Please try again.");
+                    JOptionPane.showMessageDialog(f,"Error! " + e.getMessage());
                 }
             }
         });
 
+        submitButton.setBounds(50, 180, 300, 30);
+        f.add(submitButton);
 
-        f.setSize(300,750);  
         f.setLayout(null);  
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
     }
 }
+
