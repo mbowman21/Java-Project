2757f06f506023cd74aa80a38b84e81ad40d56d4
eclnvc <eclnvc@umsystem.edu>
2023-05-10 20:04:20 -0500

    updating OverdueItems.java

--- CheckOverdues.java
+++ CheckOverdues.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.event.*;
 
 public class CheckOverdues {
@@ -10,6 +12,34 @@ public class CheckOverdues {
     public static void checkOverdues() {
         JFrame f = new JFrame("Check Overdues");
         f.setVisible(true);
+
+        JLabel memberIDLabel = new JLabel("Member ID");
+        JTextField memberidInput = new JTextField();
+
+        JButton checkButton = new JButton("Check");
+        checkButton.setBounds(50, 50, 60, 20);
+        f.add(checkButton);
+        checkButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                try{
+                    long id = Integer.parseInt(memberIDLabel.getText());
+                    if (id < 0)
+                        throw new Exception();
+                    JPanel panel = new JPanel();
+                    OverdueBooksTable table = new OverdueBooksTable();
+                    JScrollPane scrollPane = new JScrollPane(table.getTable());
+                    panel.add(table);
+                    f.setContentPane(panel);
+                    ArrayList<Item> item = new ArrayList<>();
+                    table.setOverdueBooks(item);
+                }
+                catch (Exception e) {
+                    System.out.println(e);
+                    JOptionPane.showMessageDialog(f,
+                            "Error! One of the items you inputted was formatted incorrectly. Please try again.");
+                }
+            }
+        });
 
         JButton menuButton = new JButton("Menu");
         menuButton.setBounds(0, 0, 75, 20);
--- Member.java
+++ Member.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 abstract class Member extends Person{
     protected int MemberID;
     protected float Balance;
+    protected ArrayList<Item> CheckoutHistory;
 
     public void setMemberId(int id) {
         this.MemberID = id;
@@ -16,5 +19,19 @@ abstract class Member extends Person{
 
     public float getBalance() {
         return this.Balance;
+    }
+
+    public void checkoutItem(Item i){
+        this.CheckoutHistory.add(i.itemID);
+    }
+
+    public ArrayList<Item> getOverdueList(){
+        ArrayList<Item> overdues = new ArrayList<Item>();
+        for(i in length(this.CheckoutHistory)){
+            if(this.CheckoutHistory[i].isOverdue()==true){
+                overdues.add(this.CheckoutHistory[i]);
+            }
+        }
+        return overdues;
     }
 }
